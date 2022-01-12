d307f9e7d37c55b025ff495ea054ad45c7b62712
bartr <bartr@microsoft.com>
2022-01-12 14:57:35 -0600

    updated to use PORT env var

--- src/Program.cs
+++ src/Program.cs
@@ -1,7 +1,16 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
-args = new string[] { "--urls", "http://*:8080/" };
+string? p = Environment.GetEnvironmentVariable("PORT");
+
+if (string.IsNullOrWhiteSpace(p))
+{
+    p = "8080";
+    Environment.SetEnvironmentVariable("PORT", p);
+}
+
+// pass the port
+args = new string[] { "--urls", $"http://*:{p}/" };
 
 var builder = WebApplication.CreateBuilder(args);
 
