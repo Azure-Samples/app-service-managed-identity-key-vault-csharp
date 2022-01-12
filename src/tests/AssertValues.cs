93f6ef3aa7d9a685b4517c4a1baa20407dcdfe35
bartr <bartr@microsoft.com>
2022-01-12 00:31:03 -0600

    removed old source

--- src/tests/AssertValues.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace UnitTests
-{
-    public class AssertValues
-    {
-        public static readonly string HealthzResult = "Healthy";
-        public static readonly string SecretResult = "Hello from Key Vault and Managed Identity!";
-    }
-}
