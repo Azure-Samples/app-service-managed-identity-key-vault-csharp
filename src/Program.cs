b068e477f4301457c763ad913c4da34edb844958
bartr <bartr@microsoft.com>
2022-01-12 21:39:36 -0600

    added version handler

--- src/Program.cs
+++ src/Program.cs
@@ -1,6 +1,11 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
+using System.Globalization;
+using System.Reflection;
+using System;
+using System.Text;
+
 string? p = Environment.GetEnvironmentVariable("PORT");
 
 if (string.IsNullOrWhiteSpace(p))
@@ -22,6 +27,27 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+// handle /version
+app.Use(async (context, next) =>
+{
+    // matches /version
+    if (context.Request.Path.Value == "/version")
+    {
+        // return the version info
+        context.Response.ContentType = "text/plain";
+
+        if (Attribute.GetCustomAttribute(Assembly.GetExecutingAssembly(), typeof(AssemblyInformationalVersionAttribute)) is AssemblyInformationalVersionAttribute v)
+        {
+            await context.Response.Body.WriteAsync(Encoding.UTF8.GetBytes(v.InformationalVersion)).ConfigureAwait(false);
+        }
+    }
+    else
+    {
+        // not a match, so call next middleware handler
+        await next().ConfigureAwait(false);
+    }
+});
 
 app.UseSwagger();
 app.UseSwaggerUI();
