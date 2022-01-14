fc9329818d3dc50aae0875e81da7486d48768e20
Bart Robertson <bartr@microsoft.com>
2022-01-14 07:42:36 +0000

    updated codespaces

--- .devcontainer/on-create.sh
+++ .devcontainer/on-create.sh
@@ -3,11 +3,6 @@
 echo "on-create start" >> $HOME/status
 
 # run dotnet restore
-dotnet restore src/app/mikv.csproj
-dotnet restore src/tests/mikv-tests.csproj
-
-# pull base docker images
-docker pull mcr.microsoft.com/dotnet/aspnet:6.0-alpine
-docker pull mcr.microsoft.com/dotnet/sdk:6.0
+dotnet restore src
 
 echo "on-create complete" >> $HOME/status
--- .devcontainer/post-start.sh
+++ .devcontainer/post-start.sh
@@ -5,11 +5,6 @@ echo "post-start start" >> $HOME/status
 # this runs each time the container starts
 
 # run dotnet restore
-dotnet restore src/app/mikv.csproj
-dotnet restore src/tests/mikv-tests.csproj
-
-# update the base docker images
-docker pull mcr.microsoft.com/dotnet/aspnet:6.0-alpine
-docker pull mcr.microsoft.com/dotnet/sdk:6.0
+dotnet restore src
 
 echo "post-start complete" >> $HOME/status
